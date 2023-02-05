5eef64661baf65664e4572b682e3f06e60ae08df
wyuan19asu <wyuan19@asu.edu>
2023-02-04 21:11:07 -0700

    created search bar input

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Search() {
+    return (
+        <div className="search">
+            <div className="search__bar">
+                <input type="text" placeholder='Search' />
+            </div>
+            <div className="user__chats">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
