c5428c201eb18d782810214f4843cb2736afc4fa
wyuan19asu <wyuan19@asu.edu>
2023-02-04 20:58:50 -0700

    added routes, sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Register from './pages/Register'
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import "./style.scss";
+
 function App() {
-
-
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
