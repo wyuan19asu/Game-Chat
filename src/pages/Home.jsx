8c77db54ce6cdfd34ea7ff4ab1a7a750d5169782
wyuan19asu <wyuan19@asu.edu>
2023-02-04 21:10:35 -0700

    added sidebar/chat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar';
+import Chat from '../components/Chat';
 export default function Home() {
     return (
         <div className="home">
             <div className="container">
-           
+                <Sidebar />
+                <Chat />
             </div>
         </div>
     )
