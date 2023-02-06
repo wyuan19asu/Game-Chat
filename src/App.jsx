3a4ad0c34216f206214e0f28d9aa38af60b5b996
wyuan19asu <wyuan19@asu.edu>
2023-02-05 23:06:36 -0700

    added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import "./style.scss";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-library.add(faRightFromBracket);
+import { faRightFromBracket, faVideo, faUserPlus, faEllipsis } from '@fortawesome/free-solid-svg-icons';
+library.add(faRightFromBracket, faVideo, faUserPlus, faEllipsis);
 
 function App() {
   return (
