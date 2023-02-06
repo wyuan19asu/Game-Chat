b03ec064244ef0ec80c8ebebae1a0bcd535d1ff9
wyuan19asu <wyuan19@asu.edu>
2023-02-05 23:06:58 -0700

    top chat bar layout

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default function () {
+export default function Chat() {
     return (
-        <div className="chat">chat</div>
+        <div className="chat__info">
+            <div className="chat">
+                <span>Ryan</span>
+                <div className="chat__icons">
+                    <FontAwesomeIcon icon="fa-solid fa-video" className='chat__icon' />
+                    <FontAwesomeIcon icon="fa-solid fa-user-plus" className='chat__icon' />
+                    <FontAwesomeIcon icon="fa-solid fa-ellipsis" className='chat__icon' />
+                </div>
+            </div>
+        </div>
     )
 }
