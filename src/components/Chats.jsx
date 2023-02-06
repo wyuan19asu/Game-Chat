09afa87d73f54ff4e15ded369e789ec7b049d77e
wyuan19asu <wyuan19@asu.edu>
2023-02-05 23:19:14 -0700

    importm finished side chat layout

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import profilePic from '../images/RR.jpg'
 
 export default function Chats() {
   return (
     <div className="side__chats">
-        chats
+      <div className="user__chat">
+        <img src={profilePic} alt="" className='userPic' />
+        <div className="user__chat--info">
+          <span>Ryan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user__chat">
+        <img src={profilePic} alt="" className='userPic' />
+        <div className="user__chat--info">
+          <span>Ryan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user__chat">
+        <img src={profilePic} alt="" className='userPic' />
+        <div className="user__chat--info">
+          <span>Ryan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="user__chat">
+        <img src={profilePic} alt="" className='userPic' />
+        <div className="user__chat--info">
+          <span>Ryan</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
-    )
+  )
 }
