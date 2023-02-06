447b0bb41c85efd0808518b2a74f6b7d44ae6109
wyuan19asu <wyuan19@asu.edu>
2023-02-05 23:20:07 -0700

    user chat info layout

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import profilePic from '../images/RR.jpg'
 
 export default function Search() {
     return (
@@ -6,8 +7,11 @@ export default function Search() {
             <div className="search__bar">
                 <input type="text" placeholder='Search' />
             </div>
-            <div className="user__chats">
-                <img src="" alt="" />
+            <div className="user__chat">
+                <img src={profilePic} alt="" className='userPic' />
+                <div className="user__chat--info">
+                    <span>Ryan</span>
+                </div>
             </div>
         </div>
     )
