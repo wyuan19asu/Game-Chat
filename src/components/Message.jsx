ae6e45f4091ad5448dd0faa66acb098b92d0a548
wyuan19asu <wyuan19@asu.edu>
2023-02-06 20:24:30 -0700

    finished design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import stockProfile from '../images/messagestock.jpg'
+import RR from '../images/RR.jpg'
 export default function Message() {
     return (
-        <div>Message</div>
+        <div className="message owner">
+            <div className="message__info">
+                <img src={stockProfile} alt="" className='stockProfile__img' />
+                <span>Just now</span>
+            </div>
+            <div className="message__content">
+                <p>hello</p>
+                <img src={RR} alt="" />
+            </div>
+        </div>
     )
 }
