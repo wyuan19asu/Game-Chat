09b364af47aa4a090c056fd5e2ed6f7343381889
wyuan19asu <wyuan19@asu.edu>
2023-02-07 04:01:04 -0700

    initial

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { createLogger } from "vite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQKP3VXebVP5ADsbVlbixEpmIXSEA7BWk",
+  authDomain: "gamechat-7b44f.firebaseapp.com",
+  projectId: "gamechat-7b44f",
+  storageBucket: "gamechat-7b44f.appspot.com",
+  messagingSenderId: "962453569826",
+  appId: "1:962453569826:web:e898e8517efd646514a57b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
