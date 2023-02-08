5173dbffddf8ec6770dd1a798483489eb6cf96a4
wyuan19asu <wyuan19@asu.edu>
2023-02-08 02:56:37 -0700

    added usecontext to show user connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import React, {useContext} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./style.scss";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faRightFromBracket, faVideo, faUserPlus, faEllipsis, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import { faFile, faFileImage } from '@fortawesome/free-regular-svg-icons';
-library.add(faRightFromBracket, faVideo, faUserPlus, faEllipsis, faFile, faFileImage, faPaperPlane);
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faRightFromBracket,
+  faVideo,
+  faUserPlus,
+  faEllipsis,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
+import { faFile, faFileImage } from "@fortawesome/free-regular-svg-icons";
+library.add(
+  faRightFromBracket,
+  faVideo,
+  faUserPlus,
+  faEllipsis,
+  faFile,
+  faFileImage,
+  faPaperPlane
+);
+import { AuthContext } from "./components/AuthContext";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
+  
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/register' element={<Register />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
