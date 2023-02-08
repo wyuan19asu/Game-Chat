ff0d8c1dd665bf01696eb837c47e52b3eafcbf57
wyuan19asu <wyuan19@asu.edu>
2023-02-08 03:02:30 -0700

    working signout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function Navbar() {
-    return (
-        <nav >
-            <div className="nav__container">
-                <div className="logo__div">
-                    <p className="logo">Game Chat</p>
-                </div>
-                <div className="user">
-                    <img src="" alt="" />
-                    <span>John</span>
-                    <button className="logout__btn">
-                        <FontAwesomeIcon icon="fa-solid fa-right-from-bracket" className='logout__icon' />
-                    </button>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="nav__container">
+        <div className="logo__div">
+          <p className="logo">Game Chat</p>
+        </div>
+        <div className="user">
+          <img src="" alt="" />
+          <span>John</span>
+          <button className="logout__btn" onClick={() => signOut(auth)}>
+            <FontAwesomeIcon
+              icon="fa-solid fa-right-from-bracket"
+              className="logout__icon"
+            />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
