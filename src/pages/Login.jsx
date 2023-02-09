4d7819f981235246311ef61e9fba5c4254efacc2
wyuan19asu <wyuan19@asu.edu>
2023-02-09 02:46:01 -0700

    login auth check and nav

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 export default function Login() {
-    return (
-        <div className="form__container">
-            <div className="form__wrapper">
-                <span className="logo">Game Chat</span>
-                <span className="title">Sign in</span>
-                <form action="" className="register__form">
-                    <label >
-                        Email
-                        <input type="email" placeholder='Email' id="" />
-                    </label>
-                    <label >
-                        Password
-                        <input type="password" placeholder='Must be at least 8 characters' id="password" />
-                    </label>
-                    <button className="login__btn">Login</button>
-                </form>
-                <div className="alt__register--wrapper">
-                    <div className="left__line"></div>
-                    <p className="alt__register--text"> Or Sign in with </p>
-                    <div className="right__line"></div>
-                </div>
-                <p id="login__text">Don't have an account? <Link to="/register">Register</Link></p>
-            </div>
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className="form__container">
+      <div className="form__wrapper">
+        <span className="logo">Game Chat</span>
+        <span className="title">Sign in</span>
+        <form action="" className="register__form" onSubmit={handleSubmit}>
+          <label>
+            Email
+            <input type="email" placeholder="Email" id="" />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              placeholder="Must be at least 8 characters"
+              id="password"
+            />
+          </label>
+          <button className="login__btn">Login</button>
+          {err && <span id="somethingWrong">Something went wrong</span>}
+        </form>
+        <div className="alt__register--wrapper">
+          <div className="left__line"></div>
+          <p className="alt__register--text"> Or Sign in with </p>
+          <div className="right__line"></div>
         </div>
-    )
+        <p id="login__text">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
