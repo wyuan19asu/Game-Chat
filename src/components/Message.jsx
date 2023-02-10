6d8ba2fcde5cab146ace0121ed7ddeb86f8b3939
wyuan19asu <wyuan19@asu.edu>
2023-02-09 22:24:40 -0700

    dynamic messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import stockProfile from '../images/messagestock.jpg'
-import RR from '../images/RR.jpg'
-export default function Message() {
-    return (
-        <div className="message owner">
-            <div className="message__info">
-                <img src={stockProfile} alt="" className='stockProfile__img' />
-                <span>Just now</span>
-            </div>
-            <div className="message__content">
-                <p>hello</p>
-                <img src={RR} alt="" />
-            </div>
-        </div>
-    )
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import stockProfile from "../images/messagestock.jpg";
+import RR from "../images/RR.jpg";
+export default function Message({ message }) {
+  const { currentUser } = useContext(AuthContext);
+  //   other user
+  const { data } = useContext(ChatContext);
+
+  return (
+    <div className="message owner">
+      {/* <div className="message__info">
+        <img src={stockProfile} alt="" className="stockProfile__img" />
+        <span>Just now</span>
+      </div>
+      <div className="message__content">
+        <p>hello</p>
+        <img src={RR} alt="" />
+      </div> */}
+    </div>
+  );
 }
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import Message from './Message';
+import React, { useContext, useEffect, useState } from "react";
+import Message from "./Message";
+import { ChatContext } from "../context/ChatContext";
+import { onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { doc } from "firebase/firestore";
 
 export default function Messages() {
-    return (
-        <div className="messages">
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-        </div>
-    )
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
+
+  return (
+    <div className="messages">
+      {messages.map((message) => (
+        <Message message={message} key={message.id} />
+      ))}
+    </div>
+  );
 }
