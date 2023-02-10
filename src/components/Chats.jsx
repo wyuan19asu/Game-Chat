7057ed8d7fe8e0c64631c281d405e83e38350a76
wyuan19asu <wyuan19@asu.edu>
2023-02-09 20:23:17 -0700

    added dynamic chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,37 +1,53 @@
-import React from 'react'
-import profilePic from '../images/RR.jpg'
+import React, { useContext, useEffect, useState } from "react";
+import profilePic from "../images/RR.jpg";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 export default function Chats() {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
   return (
     <div className="side__chats">
-      <div className="user__chat">
-        <img src={profilePic} alt="" className='userPic' />
-        <div className="user__chat--info">
-          <span>Ryan</span>
-          <p>Hello</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div
+          className="user__chat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
+          <img src={chat[1].userInfo.photoURL} alt="" className="userPic" />
+          <div className="user__chat--info">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+      ))}
       <div className="user__chat">
-        <img src={profilePic} alt="" className='userPic' />
-        <div className="user__chat--info">
-          <span>Ryan</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user__chat">
-        <img src={profilePic} alt="" className='userPic' />
-        <div className="user__chat--info">
-          <span>Ryan</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user__chat">
-        <img src={profilePic} alt="" className='userPic' />
+        <img src={profilePic} alt="" className="userPic" />
         <div className="user__chat--info">
           <span>Ryan</span>
           <p>Hello</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
