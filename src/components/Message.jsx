861b3eb90f6c84948a0de7edb4426617d809461f
wyuan19asu <wyuan19@asu.edu>
2023-02-09 23:08:07 -0700

    added dynamic msgs, imgs

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,23 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
-import stockProfile from "../images/messagestock.jpg";
-import RR from "../images/RR.jpg";
+
 export default function Message({ message }) {
   const { currentUser } = useContext(AuthContext);
   //   other user
   const { data } = useContext(ChatContext);
 
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className="message owner">
-      {/* <div className="message__info">
-        <img src={stockProfile} alt="" className="stockProfile__img" />
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
+      <div className="message__info">
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+          className="stockProfile__img"
+        />
         <span>Just now</span>
       </div>
       <div className="message__content">
-        <p>hello</p>
-        <img src={RR} alt="" />
-      </div> */}
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
     </div>
   );
 }
