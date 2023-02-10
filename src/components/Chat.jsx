81a1cf934ac48c063b9567ff98c0b04066f006c8
wyuan19asu <wyuan19@asu.edu>
2023-02-09 22:23:44 -0700

    dynamic user name for active chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Messages from './Messages';
-import Input from './Input';
+import React, { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 export default function Chat() {
-    return (
-        <div className="chat__info">
-            <div className="chat">
-                <span>Ryan</span>
-                <div className="chat__icons">
-                    <FontAwesomeIcon icon="fa-solid fa-video" className='chat__icon' />
-                    <FontAwesomeIcon icon="fa-solid fa-user-plus" className='chat__icon' />
-                    <FontAwesomeIcon icon="fa-solid fa-ellipsis" className='chat__icon' />
-                </div>
-            </div>
-            <Messages />
-            <Input />
+  const { data } = useContext(ChatContext);
+  return (
+    <div className="chat__info">
+      <div className="chat">
+        <span>{data.user?.displayName}</span>
+        <div className="chat__icons">
+          <FontAwesomeIcon icon="fa-solid fa-video" className="chat__icon" />
+          <FontAwesomeIcon
+            icon="fa-solid fa-user-plus"
+            className="chat__icon"
+          />
+          <FontAwesomeIcon icon="fa-solid fa-ellipsis" className="chat__icon" />
         </div>
-    )
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 }
