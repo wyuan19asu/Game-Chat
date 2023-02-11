9b98aa7896542e23e9431f7469b15aa5663ea964
wyuan19asu <wyuan19@asu.edu>
2023-02-10 21:37:32 -0700

    removed unused import

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import profilePic from "../images/RR.jpg";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
